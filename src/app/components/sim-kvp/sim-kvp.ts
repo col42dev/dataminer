d7ae1d8a009bed39108859b7feeef968b087b137
Colin Moore <colin.moore42@googlemail.com>
2015-11-01 15:45:22 +0000

    first commit

---
+++ src/app/components/sim-kvp/sim-kvp.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'sim-kvp',
+  templateUrl: 'app/components/sim-kvp/sim-kvp.html',
+  styleUrls: ['app/components/sim-kvp/sim-kvp.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class SimKVP {
+
+  constructor() {}
+
+}
