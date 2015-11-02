df185659b731f3374a65363bcbc152be2af67eef
Colin Moore <colin.moore42@googlemail.com>
2015-11-02 01:25:33 +0000

    simkvp first functional - load from myjson, import from googledocs, export to myjson

--- src/app/components/simkvp/simkvp.ts
+++ src/app/components/simkvp/simkvp.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/angular2';
+import {Http, Headers} from 'angular2/http'
 import {RouteParams} from 'angular2/router';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Location} from 'angular2/router';
@@ -13,11 +14,93 @@ import {Location} from 'angular2/router';
 })
 export class Simkvp {
 
-     id: string;
-    constructor(params: RouteParams){
-        this.id = params.get('id');
+    private result: Object;
+    private http: Http;
+    private myJsonUrl: string = 'https://api.myjson.com/bins/2zkwu';
+    private googleDocJsonFeedUrl: string ='https://spreadsheets.google.com/feeds/list/1xP0aCx9S4wG_3XN9au5VezJ6xVTnZWNlOLX8l6B69n4/otw4nb/public/values?alt=json';
+   
+    // 
+    constructor(params: RouteParams, http: Http){
+        this.http = http;
+        this.importFromMyJSON();
+    }
+    
+    importFromMyJSON() {  
+      console.log('importFromMyJSON');
+      
+      this.result = { 'json':{}, 'text':'loading...'};
+     
+      this.http
+        .get(this.myJsonUrl)
+        .map(res => res.json())
+        .subscribe(
+          res => this.result  = { 'json':res, 'text':JSON.stringify(res, null, 2)}
+         );
+    }
+    
+
+    importFromGoogleDocs() {  
+      console.log('importFromGoogleDocs');
+          
+      this.http
+        .get(this.googleDocJsonFeedUrl)
+        .map(res => res.json())
+        .subscribe(
+          res => this.result  = this.parseGoogleDocJSON(res)
+         );
+    }
+    
+    exportToMyJSON() {
+        console.log('exportToMyJSON');
+        
+        var formatted = this.result['json'];
+        formatted['title'] = 'simvalues';
+        
+ 
+        var newVersionIdArray = [];
+        if ( formatted.hasOwnProperty('version')) {
+          newVersionIdArray = formatted['version'].split('.');
+        } else {
+          newVersionIdArray = ['0', '0', '0'];
+        } 
+        newVersionIdArray[2] = parseInt(newVersionIdArray[2], 10) + 1;
+        formatted['version'] = newVersionIdArray.join('.'); 
+        formatted['lastEditDate'] = (new Date()).toString();
+        
+        this.result['json'] = formatted;
+        this.result['text'] = JSON.stringify(formatted, null, 2);
+        
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json; charset=utf-8');
+
+        let data: string = JSON.stringify(formatted, null, 2);
+        this.http.put(this.myJsonUrl, data, { headers: headers}) 
+          .map(res => res.json())
+          .subscribe(
+            //data => this.result['text'],
+            err => console.log(err),
+            () => console.log('Authentication Complete')
+          ); 
     }
     
     
+    
+    parseGoogleDocJSON(res) {
+      var simvalues = this.result['json'];
+      simvalues['globals'] = {};
+
+      for (var rowIndex = 0; rowIndex < res.feed.entry.length; rowIndex++) { 
+        var row = {};
+        var key = res.feed.entry[rowIndex]['gsx$key'].$t;
+
+        var value = res.feed.entry[rowIndex]['gsx$value'].$t;
+        if (!isNaN(value)) {
+            value = parseInt( value, 10);
+        }
+        simvalues['globals'][key] = value;
+      }
+       
+      return { 'json':simvalues, 'text':JSON.stringify(simvalues, null, 2)};
+    }
 
 }
