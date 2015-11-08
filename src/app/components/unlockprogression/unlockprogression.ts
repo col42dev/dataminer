4b51fc478147a407a2ea433c9a8b94029887120d
Colin Moore <colin.moore42@googlemail.com>
2015-11-08 22:45:26 +0000

    progression unlocks displayed as table

--- src/app/components/unlockprogression/unlockprogression.ts
+++ src/app/components/unlockprogression/unlockprogression.ts
@@ -3,13 +3,15 @@ import {Http, Headers} from 'angular2/http'
 import {RouteParams} from 'angular2/router';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Location} from 'angular2/router';
+import { Grid } from '../grid/grid';
+import { Column } from '../grid/column';
 
 @Component({
   selector: 'unlockprogression',
   templateUrl: 'app/components/unlockprogression/unlockprogression.html',
   styleUrls: ['app/components/unlockprogression/unlockprogression.css'],
   providers: [],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, Grid],
   pipes: []
 })
 export class Unlockprogression {
@@ -18,7 +20,9 @@ export class Unlockprogression {
     private http: Http;
     private myJsonUrl: string = 'https://api.myjson.com/bins/28kay?pretty=1';
     private googleDocJsonFeedUrl: string ='https://spreadsheets.google.com/feeds/list/1xP0aCx9S4wG_3XN9au5VezJ6xVTnZWNlOLX8l6B69n4/oz4n58j/public/values?alt=json';
-   
+    private columns: Array<Column>;
+    private categories;
+       
     // 
     constructor(params: RouteParams, http: Http){
         this.http = http;
@@ -33,10 +37,16 @@ export class Unlockprogression {
         .get(this.myJsonUrl)
         .map(res => res.json())
         .subscribe(
-          res => this.result  = { 'json':res, 'text':JSON.stringify(res, null, 2)}
+           res =>  this.populateResult( res)
          );
     }
     
+    populateResult( res) {
+      this.result = { 'json':res, 'text':JSON.stringify(res, null, 2)};
+            this.columns = this.getColumns();
+      this.categories = this.getCategories();
+
+    }
 
     importFromGoogleDocs() {  
       console.log('importFromGoogleDocs');
@@ -96,10 +106,13 @@ export class Unlockprogression {
       for (var i = 0; i < res.feed.entry.length; i++) {  
         let subcategory = res.feed.entry[i].gsx$subcategory.$t;
         let amounts = [];
-        for ( let level = 1; level <= 50; level ++) {
+        
+        let level = 1;
+        while (res.feed.entry[i].hasOwnProperty('gsx$p'+(level))) { 
           let propnameLevel = 'gsx$p' + level;
           let amount = parseInt(res.feed.entry[i][propnameLevel].$t, 10); 
           amounts.push( amount);
+          level += 1;
         }
         progressions[subcategory] = amounts;
       }
@@ -111,4 +124,41 @@ export class Unlockprogression {
       return { 'json':rootNode, 'text':JSON.stringify(rootNode, null, 2)};
     }
 
+
+   getColumns(): Array<Column> {
+            
+        var colKeys = [];    
+        
+        let levels = this.result['json']['data'][ Object.keys(this.result['json']['data'])[0]].length;
+        
+        for ( let level = 0; level <= levels; level ++) {
+          colKeys.push(level);
+        }
+       
+        var thisColumns = [];
+        colKeys.forEach( function( thisKey) {
+            if (thisKey == '0') {
+              thisColumns.push( new Column( thisKey, 'category'));
+            } else {
+              thisColumns.push( new Column( thisKey, thisKey));
+            }
+          }.bind( this));
+     
+        return thisColumns;
+    }
+    
+    
+    getCategories() {
+        var thisCharacters = [];
+        
+        let rowKeys = Object.keys(this.result['json']['data']);
+        rowKeys.forEach( function( thisKey) {            
+            var row = [];
+            row.push(thisKey);
+            row = row.concat(this.result['json']['data'][thisKey]);
+            thisCharacters.push( row);
+        }.bind( this));
+     
+        return thisCharacters;
+    }
 }
