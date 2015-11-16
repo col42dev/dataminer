361a8f31c1640e8dd89106aea808aef88a76253e
Colin Moore <colin.moore42@googlemail.com>
2015-11-16 17:51:49 +0000

    factored versioning functionality in to a component and added as a provider to each exporter. Each exporter now runs a version verify check prior to export and returns a fail message if you do not have the latest data miner version loaded.

--- publishToEC2.sh
+++ publishToEC2.sh
@@ -1,16 +1,3 @@
 echo Publishing
 #scp -r -i ../AWS/cmoore2.pem ./dist/. ec2-user@ec2-54-201-237-107.us-west-2.compute.amazonaws.com:~/nginx/html/dataminers/.
 
-var=$(python readversion.py)
-echo $var
-
-a='{"dataminer":{"liveVersion":"'
-b='"}}'
-c=$a$var$b
-
-
-curl \
---header "Content-type: application/json; charset=utf-8" \
---request PUT \
---data $c \
-https://api.myjson.com/bins/1t5wx
