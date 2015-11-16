1cada76bc5aabb6eb6f2189d99d342a321b11a6f
Rajan Tande <rajan.tande@gmail.com>
2015-11-16 12:08:19 -0800

    Comment and whitespace fixes

--- src/app/components/playerpowers/playerpowers.ts
+++ src/app/components/playerpowers/playerpowers.ts
@@ -72,12 +72,11 @@ export class Playerpowers {
     playerPowersValues['version'] = version;
     playerPowersValues['lastEditDate'] = lastEditDate;
 
-    // What is this doing?
     for (var rowIndex = 0; rowIndex < res.feed.entry.length; rowIndex++) { 
       var row: Object = {};
       
       let rowKeys = Object.keys(res.feed.entry[rowIndex]);
-      rowKeys.forEach( function( thisKey) {
+      rowKeys.forEach( function(thisKey) {
         if (thisKey.indexOf('gsx$') === 0 && thisKey !== 'gsx$charactertype') {
             let truncatedKeyName = thisKey.replace('gsx$', '');
 
