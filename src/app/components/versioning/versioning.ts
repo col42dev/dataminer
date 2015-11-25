89a3192957b7cc19eed25f4f325e5c3ccfcb0081
Colin Moore <colin.moore42@googlemail.com>
2015-11-25 13:47:55 +0000

    version increment

--- src/app/components/versioning/versioning.ts
+++ src/app/components/versioning/versioning.ts
@@ -13,7 +13,7 @@ export class Versioning {
   // Stores dataminer app versioning string on myJSON.
 
   http: Http;
-  version = '0.0.45';
+  version = '0.0.47';
   liveVersion = '';
   hasLatest:number = 0;
   private verifiedCallback:Function = null;
