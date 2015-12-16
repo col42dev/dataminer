387b68471bd3462f8688f1e6ddf757fe043235f0
Colin Moore <colin.moore42@googlemail.com>
2015-12-16 19:04:02 +0000

    publish

--- PTownPublish.sh
+++ PTownPublish.sh
@@ -1,4 +1,4 @@
-scp -r -i ../AWS/PTownPerforce.pem ./dist/. ec2-user@ec2-54-67-81-203.us-west-1.compute.amazonaws.com:/usr/share/nginx/html/dataminer/.
+scp -r -i ../AWS/PTownPerforce.pem ./. ec2-user@ec2-54-67-81-203.us-west-1.compute.amazonaws.com:/usr/share/nginx/html/dataminer/.
 
 var=$(python readversion.py)
 echo $var
